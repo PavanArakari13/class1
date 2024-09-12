df88a28a8974d67f21a79a1f5324f08665792453
PavanArakari13 <b13pavan@gmail.com>
2024-09-12 10:33:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,6 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://w
  
 // Firebase config 
 <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBR5OQQt63faW_MeHquseA_6ADe-VHnvtA",
     authDomain: "class1-7f81c.firebaseapp.com",
